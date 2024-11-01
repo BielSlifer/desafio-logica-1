9ede1e0a7a9e4bb1656cc1be0970b5a77d21afb6
BielSlifer <biel161760redes@gmail.com>
2024-11-01 19:56:50 -0300

    identação melhorada

--- desafio.js
+++ desafio.js
@@ -5,16 +5,16 @@ if (XP < 1000) {
     console.log("O Herói de nome " + nome + " está no nível Ferro");
 } else if (XP >= 1000 && XP <= 2000) { 
     console.log("O Herói de nome " + nome + " está no nível Bronze");
-}else if (XP >= 2001 && XP <= 5000) { 
+} else if (XP >= 2001 && XP <= 5000) { 
     console.log("O Herói de nome " + nome + " está no nível Prata");
-}else if (XP >= 5001 && XP <= 7000) { 
+} else if (XP >= 5001 && XP <= 7000) { 
     console.log("O Herói de nome " + nome + " está no nível Ouro");
-}else if (XP >= 7001 && XP <= 8000) { 
+} else if (XP >= 7001 && XP <= 8000) { 
     console.log("O Herói de nome " + nome + " está no nível Platina");
-}else if (XP >= 8001 && XP <= 9000) { 
+} else if (XP >= 8001 && XP <= 9000) { 
     console.log("O Herói de nome " + nome + " está no nível Ascendente");
-}else if (XP >= 9001 && XP <= 10000) { 
+} else if (XP >= 9001 && XP <= 10000) { 
     console.log("O Herói de nome " + nome + " está no nível Imortal");
-}else if (XP >= 10001) { 
+} else if (XP >= 10001) { 
     console.log("O Herói de nome " + nome + " está no nível Radiante");
 }
